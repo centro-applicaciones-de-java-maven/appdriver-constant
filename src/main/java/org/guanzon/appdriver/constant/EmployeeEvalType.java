0dea51021f71582f15f324f3d9662305ee9eb71d
xurpas7 <83201804+mtcuison07@users.noreply.github.com>
2024-08-05 17:18:10 +0800

    2024-08-05

---
+++ src/main/java/org/guanzon/appdriver/constant/EmployeeEvalType.java
@@ -0,0 +1,6 @@
+package org.guanzon.appdriver.constant;
+
+public class EmployeeEvalType {
+    public static final String FOR_PROBATIONARY = "0";
+    public static final String FOR_REGULAR_EOC = "1";
+}
---
+++ src/main/java/org/guanzon/appdriver/constant/EmployeeType.java
@@ -0,0 +1,8 @@
+package org.guanzon.appdriver.constant;
+
+public class EmployeeType {
+    public static final String AGENCY = "A";
+    public static final String CASUAL = "C";
+    public static final String PROBATIONARY = "P";
+    public static final String REGULAR = "R";
+}
