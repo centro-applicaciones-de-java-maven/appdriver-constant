048f13f3c2d451ac8c327156fec5cfc446356e9f
xurpas7 <michael_cuison07@yahoo.com>
2024-12-09 16:17:21 +0800

    20241209

--- src/main/java/org/guanzon/appdriver/constant/SQLCondition.java
+++ src/main/java/org/guanzon/appdriver/constant/SQLCondition.java
@@ -1,13 +1,13 @@
 package org.guanzon.appdriver.constant;
 
 public class SQLCondition {
-    public static final String NONE = "0";
-    public static final String LIKE = "1";
-    public static final String NOT_LIKE = "2";
-    public static final String EQUALS = "3";
-    public static final String NOT_EQUAL_TO = "4";
-    public static final String GREATER_THAN = "5";
-    public static final String GREATER_THAN_OR_EQUAL = "7";
-    public static final String LESS_THAN = "8";
-    public static final String LESS_THAN_OR_EQUAL = "9";
+    public static final int NONE = 0;
+    public static final int LIKE = 1;
+    public static final int NOT_LIKE = 2;
+    public static final int EQUALS = 3;
+    public static final int NOT_EQUAL_TO = 4;
+    public static final int GREATER_THAN = 5;
+    public static final int GREATER_THAN_OR_EQUAL = 6;
+    public static final int LESS_THAN = 7;
+    public static final int LESS_THAN_OR_EQUAL = 8;
 }
