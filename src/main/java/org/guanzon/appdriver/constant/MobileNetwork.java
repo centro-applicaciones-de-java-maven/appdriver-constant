fd59cda138eef9451b4bec01f5c41663ee53b81f
xurpas7 <michael_cuison07@yahoo.com>
2024-12-13 17:14:52 +0800

    20241213

---
+++ src/main/java/org/guanzon/appdriver/constant/MobileNetwork.java
@@ -0,0 +1,9 @@
+package org.guanzon.appdriver.constant;
+
+public class MobileNetwork {
+    public static final String GLOBE = "0";
+    public static final String SMART = "1";
+    public static final String SUN = "2";
+    public static final String DITO = "3";
+    public static final String OTHERS = "";
+}
