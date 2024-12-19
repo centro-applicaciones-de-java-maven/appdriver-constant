2cfe01a2220754e2ba00f6035fa9b91e55a44188
xurpas7 <michael_cuison07@yahoo.com>
2024-12-19 16:57:05 +0800

    20241219

---
+++ src/main/java/org/guanzon/appdriver/constant/InventoryClassification.java
@@ -0,0 +1,10 @@
+package org.guanzon.appdriver.constant;
+
+public class InventoryClassification {
+    public static final String VERY_FAST_MOVING = "A";
+    public static final String FAST_MOVING = "B";
+    public static final String MOVING = "C";
+    public static final String SLOW_MOVING = "D";
+    public static final String VERY_SLOW_MOVING = "E";
+    public static final String NEW_ITEMS = "F";
+}
